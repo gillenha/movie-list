7d5cad7a2c3602c4a7fe15878c91e9289105b1f3
gillenha <gillen08@gmail.com>
2018-11-28 23:02:44 -0500

    fleshed out the AddMovie component

--- src/components/addMovie.jsx
+++ src/components/addMovie.jsx
@@ -3,7 +3,50 @@ import React from "react";
 const AddMovie = ({ match, history }) => {
   return (
     <div>
-      <h1>Add Movie {match.params.id}</h1>
+      <h1>Add Movie</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor="exampleFormControlInput1">Title</label>
+          <input
+            type="text"
+            className="form-control"
+            id="title"
+            placeholder="Movie Title"
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="exampleFormControlSelect1">Genre</label>
+          <select className="form-control" id="exampleFormControlSelect1">
+            <option>Action</option>
+            <option>Comedy</option>
+            <option>Thriller</option>
+            <option>Drama</option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleFormControlInput1">Number in Stock</label>
+          <input
+            type="number"
+            className="form-control"
+            id="stock"
+            placeholder="Max: 100"
+            min="1"
+            max="100"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="exampleFormControlInput1">Rate</label>
+          <input
+            type="number"
+            className="form-control"
+            id="stock"
+            placeholder="Rate this movie 1-5"
+            min="1"
+            max="5"
+          />
+        </div>
+      </form>
       <button
         className="btn btn-primary"
         onClick={() => history.push("/movies")}
