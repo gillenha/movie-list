65ddd9b994db0a66cb4e27013e9d3eeaaad8a18c
gillenha <gillen08@gmail.com>
2018-11-28 23:16:07 -0500

    added a better form

--- src/components/addMovie.jsx
+++ src/components/addMovie.jsx
@@ -1,60 +1,46 @@
 import React from "react";
+import Joi from "joi-browser";
+import Form from "./common/form";
 
-const AddMovie = ({ match, history }) => {
-  return (
-    <div>
-      <h1>Add Movie</h1>
-      <form>
-        <div className="form-group">
-          <label htmlFor="exampleFormControlInput1">Title</label>
-          <input
-            type="text"
-            className="form-control"
-            id="title"
-            placeholder="Movie Title"
-          />
-        </div>
+class AddMovie extends Form {
+  state = {
+    data: { title: "", genre: "", inStock: 0, rate: 0 },
+    errors: {}
+  };
 
-        <div className="form-group">
-          <label htmlFor="exampleFormControlSelect1">Genre</label>
-          <select className="form-control" id="exampleFormControlSelect1">
-            <option>Action</option>
-            <option>Comedy</option>
-            <option>Thriller</option>
-            <option>Drama</option>
-          </select>
-        </div>
-        <div className="form-group">
-          <label htmlFor="exampleFormControlInput1">Number in Stock</label>
-          <input
-            type="number"
-            className="form-control"
-            id="stock"
-            placeholder="Max: 100"
-            min="1"
-            max="100"
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="exampleFormControlInput1">Rate</label>
-          <input
-            type="number"
-            className="form-control"
-            id="stock"
-            placeholder="Rate this movie 1-5"
-            min="1"
-            max="5"
-          />
-        </div>
-      </form>
-      <button
-        className="btn btn-primary"
-        onClick={() => history.push("/movies")}
-      >
-        Add
-      </button>
-    </div>
-  );
-};
+  schema = {
+    title: Joi.string()
+      .required()
+      .label("Title"),
+    genre: Joi.string()
+      .required()
+      .label("Genre"),
+    inStock: Joi.number()
+      .required()
+      .min(1)
+      .max(100)
+      .label("Number in stock"),
+    rate: Joi.number()
+      .required()
+      .min(1)
+      .max(5)
+      .label("Rate")
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Add Movie</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("title", "Title")}
+          {this.renderInput("genre", "Genre")}
+          {this.renderInput("inStock", "In Stock", "number")}
+          {this.renderInput("rate", "Rate", "number")}
+          {this.renderButton("Register")}
+        </form>
+      </div>
+    );
+  }
+}
 
 export default AddMovie;
